ecb42b71d544cbcd14f262687a34cb9dead445a0
Ashik Achaparambil <getashik@gmail.com>
2018-11-07 16:10:15 -0600

    latest

--- src/components/AwsList.jsx
+++ src/components/AwsList.jsx
@@ -4,13 +4,48 @@ import {DeleteUser} from '../actions/awsList';
 
 class Row extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={edit:false};
+    }
+
     delItem(){
         this.props.dispatch(DeleteUser({id:this.props.id}));
     }
 
+    pressEdit(){
+        this.setState({edit:true})
+    }
+    pressSaveItem(){
+        var obj={
+            "userName":this.refs.userName.value,
+            "email":this.refs.email.value,
+            "id":this.props.id,
+
+        }
+     }
+    inputChange(e,fld){
+    alert(e.target.value);
+    this.props[fld]=e.target.value;
+    }
+
     render(){
 
-        return(<li>{ this.props.userName} ----- {this.props.email}<button onClick={this.delItem.bind(this)}>Delete</button></li>);
+     if(this.state.edit){
+
+        return(<li>
+             <input type="text" defaultValue={this.props.userName} ref="userName"  />
+             <input type="text" defaultValue={this.props.email} ref="email"  />
+             <button onClick={this.pressSaveItem.bind(this)}>Save</button>
+            </li>);
+    }else{
+        return(<li>{ this.props.userName} ----- {this.props.email}
+        <button onClick={this.delItem.bind(this)}>Delete</button>
+        <button onClick={this.pressEdit.bind(this)}>Edit</button>
+        
+        </li>);
+    }
+    
     }
 }
 
